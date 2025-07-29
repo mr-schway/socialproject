d57ab0e252f0f8e15b1369ad20a641c735ee6f3d
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-07-29 09:56:31 +0300

    login using forms

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from users.forms import LoginForm
 
 # Create your views here.
+def user_login(request):
+  if request.method == 'POST':
+    form = LoginForm(request.POST)
+    if form.is_valid():
+      data = form.cleaned_data 
+      user = authenticate(request, username = data['username'], password = data['password'])
+      if user is not None:
+        login(request, user)
+        return HttpResponse('User authenticated and logged in')
+      else: 
+        return HttpResponse('Invalid user credentials')
+  else:
+    form = LoginForm()
+  return render(request, 'users/login.html', {'form': form})
