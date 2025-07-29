6367a1fec29e5b960de3ec3cc1bc7024e7388238
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-07-29 10:38:39 +0300

    Logout

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
 from users import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
   path('login/', views.user_login, name='login'),
+  # path('logout/', auth_views.LogoutView.as_view()), # django 4
+  path("logout/", auth_views.LogoutView.as_view(http_method_names=["post", "get", "options"],template_name='users/logout.html'), name='logout'),
 ]
