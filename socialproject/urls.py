f18b9b7c8bc5a2b0a523cdd7840f7e5444bd56f0
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-07-30 11:02:34 +0300

    Profile

--- socialproject/urls.py
+++ socialproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from users import views
 
@@ -23,3 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')), 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
